a9273a614d864dcd3b6d5d9c215441076829bada
Daniel Mallia <Daniel.Mallia21@myhunter.cuny.edu>
2022-12-11 17:28:00 -0500

    Visuals, basic agent types, cats become hungry again, add hunger plotting, add realistic map
    Co-authored-by: Artjom-Plaunov <artyemnyc@gmail.com>

--- FoodModel.py
+++ FoodModel.py
@@ -1,4 +1,17 @@
 import mesa
+from Utilities import get_locs
+
+class StreetAgent(mesa.Agent):
+    pass
+
+class BackyardAgent(mesa.Agent):
+    pass
+
+class ShopAgent(mesa.Agent):
+    pass
+
+class RestaurantAgent(mesa.Agent):
+    pass
 
 class CatAgent(mesa.Agent):
     def __init__(self, unique_id, model):
@@ -11,7 +24,7 @@ class CatAgent(mesa.Agent):
             new_loc = None
             found_food = False
             for neighbor in self.model.grid.iter_neighbors(self.pos, True):
-                if isinstance(neighbor, Household) and neighbor.food:
+                if isinstance(neighbor, HouseAgent) and neighbor.food:
                     found_food = True
                     new_loc = neighbor.pos
                     break
@@ -24,8 +37,10 @@ class CatAgent(mesa.Agent):
             if found_food:
                 self.is_hungry = False
                 #print("I ATE FOOD")
+        else:
+            self.is_hungry = self.random.choices([True, False], weights=[0.03,0.97])
 
-class Household(mesa.Agent):
+class HouseAgent(mesa.Agent):
     def __init__(self, unique_id, model):
         super().__init__(unique_id, model)
         self.food = True
@@ -33,11 +48,22 @@ class Household(mesa.Agent):
     def step(self):
         #print("House: ", self.unique_id, " has food out ", self.food, " at ", self.pos)
         if not self.food:
-            self.food = self.random.choices([True, False], weights=[0.2,0.8])
+            self.food = self.random.choices([True, False], weights=[0.03,0.97])
 
+def get_hunger(model):
+    return len([1 for agent in model.schedule.agents if isinstance(agent,CatAgent) and agent.is_hungry]) / len(model.schedule.agents)
+
+class_map = {
+    "street" : StreetAgent,
+    "house" : HouseAgent,
+    "backyard" : BackyardAgent,
+    "shop" : ShopAgent,
+    "restaurant" : RestaurantAgent
+}
 
 class FoodModel(mesa.Model):
-    def __init__(self, num_cats, num_houses, width, height, seed=1234):
+    def __init__(self, num_cats, num_houses, width=20, height=24, seed=1234):
+        self.current_id = 1
         self.num_cats = num_cats
         self.num_houses = num_houses
 
@@ -46,45 +72,133 @@ class FoodModel(mesa.Model):
         self.schedule = mesa.time.RandomActivation(self)
 
         for i in range(self.num_cats):
-            curr_a = CatAgent(i, self)
+            curr_a = CatAgent(self.next_id(), self)
             self.schedule.add(curr_a)
             x = self.random.randrange(self.grid.width)
             y = self.random.randrange(self.grid.height)
             self.grid.place_agent(curr_a, (x, y))
 
-        for i in range(self.num_cats, self.num_cats + self.num_houses):
-            curr_a = Household(i, self)
-            self.schedule.add(curr_a)
-            x = self.random.randrange(self.grid.width)
-            y = self.random.randrange(self.grid.height)
-            self.grid.place_agent(curr_a, (x, y))
+        # GRID / ENVIRONMENTAL SETUP
+        environment = get_locs(width, height)
+
+        # TODO: UPDATE THIS ACCORDINGLY
+        action_zones = ["house"]
+        for zone_type in environment:
+            schedule = zone_type in action_zones
+            for loc in environment[zone_type]:
+                curr_a = class_map[zone_type](self.next_id(), self)
+                if schedule:
+                    self.schedule.add(curr_a)
+                self.grid.place_agent(curr_a, loc)
+
+        # for i in range(self.num_houses):
+        #     curr_a = HouseAgent(self.next_id(), self)
+        #     self.schedule.add(curr_a)
+        #     x = self.random.randrange(self.grid.width)
+        #     y = self.random.randrange(self.grid.height)
+        #     self.grid.place_agent(curr_a, (x, y))
+
+        self.datacollector = mesa.DataCollector(model_reporters={"Hunger": get_hunger})
 
     def step(self):
         """Advance the model by one step."""
+        self.datacollector.collect(self)
         self.schedule.step()
 
 def agent_portrayal(agent):
-    portrayal = {
-            "Shape" : "circle",
-            "Color" : "red",
-            "Filled" : "true",
-            "Layer" : 0,
-            "r" : 0.5
-    }
 
-    if isinstance(agent, Household):
-        portrayal["Color"] = "blue"
-        portrayal["Layer"] = 1
-        portrayal["r"] = 0.2
+    # https://www.flaticon.com/free-icon/cat_220124
+    if isinstance(agent, CatAgent):
+        # portrayal = {
+        #         "Shape" : "circle",
+        #         "Color" : "red",
+        #         "Filled" : "true",
+        #         "Layer" : 1,
+        #         "r" : 0.5
+        # }
+
+        portrayal = {
+            "Shape" : "Images/cat.PNG",
+            "Layer" : 1,
+            "scale" : 0.7
+        }
+
+    if isinstance(agent, HouseAgent):
+        portrayal = {
+                "Shape" : "rect",
+                "Color" : ["#00EEEE"],
+                "Layer" : 0,
+                "w" : 0.8,
+                "h" : 0.8,
+                "Filled" : "false",
+                # "text" : "H",
+                # "text_color" : "black"
+        }
+
+    if isinstance(agent, StreetAgent):
+        portrayal = {
+                "Shape" : "rect",
+                "Color" : ["#B8B8B8"],
+                "Layer" : 0,
+                "w" : 0.8,
+                "h" : 0.8,
+                "Filled" : "false",
+        }
+
+    if isinstance(agent, BackyardAgent):
+        portrayal = {
+                "Shape" : "rect",
+                "Color" : ["#84e184", "#adebad", "#d6f5d6"],
+                "Layer" : 0,
+                "w" : 0.8,
+                "h" : 0.8,
+                "Filled" : "true",
+        }
+
+    if isinstance(agent, ShopAgent):
+        portrayal = {
+                "Shape" : "rect",
+                "Color" : "green",
+                "Layer" : 0,
+                "w" : 0.8,
+                "h" : 0.8,
+                "Filled" : "false",
+        }
+
+    # https://www.clipartmax.com/png/middle/30-303462_rat-icon-enviropest-mouse.png
+    if isinstance(agent, RestaurantAgent):
+        portrayal = {
+            "Shape" : "Images/Mouse.png",
+            "Layer" : 0,
+            "scale" : 0.5,
+        }
+        # portrayal = {
+        #         "Shape" : "rect",
+        #         "Color" : "yellow",
+        #         "Layer" : 0,
+        #         "w" : 0.8,
+        #         "h" : 0.8,
+        #         "Filled" : "false",
+        # }
+
+        # portrayal = {
+        #         "Shape" : "House.png",
+        #         "Layer" : 0,
+        #         "scale" : 0.7
+        # }
 
     return portrayal
 
 
 if __name__ == "__main__":
-    grid = mesa.visualization.CanvasGrid(agent_portrayal, 20,20,500,500)
+    # MAKE THESE INTO ARGUMENTS 
+    WIDTH = 20
+    HEIGHT = 24
+    grid = mesa.visualization.CanvasGrid(agent_portrayal, WIDTH,HEIGHT,1000,1000)
+    chart = mesa.visualization.ChartModule([{"Label" : "Hunger", "Color" : "Black"}])
     server = mesa.visualization.ModularServer(
-        FoodModel, [grid], "Food Model",
-        {"num_cats" : 100, "num_houses" : 5, "width" : 20, "height" : 20}
+        FoodModel, [grid,chart], "Food Model",
+        {"num_cats" : 10, "num_houses" : 5, "width" : WIDTH, "height" : HEIGHT}
     )
     server.launch()
     # model = FoodModel(5,5,20,20)
---
+++ Utilities.py
@@ -0,0 +1,46 @@
+import random
+from collections import defaultdict
+
+# SCALE NOTES:
+# We are focusing around house lots - a street including sidewalks is
+# approximately the size of a lot, as is the distance between houses (across
+# their backyards). Thus a block is considered 3 lots wide (house, backyards
+# house) followed by a street and then another block...
+
+# Lots between avenues (inclusive of backyard of BOTH top and bottom avenue
+# shops/restaurants)
+#LOTS_BETWEEN_AVENUES = 24
+
+# # Lots between streets (house, backyards, house)
+# LOTS_BETWEEN_STREETS = 3
+
+# GRID width controls the number of blocks you'll generate
+#GRID_WIDTH = 20
+
+# NOTE FROM MESA DOCS:
+# Grid cells are indexed by [x][y], where [0][0] is assumed to be the
+# bottom-left and [width-1][height-1] is the top-right. If a grid is toroidal,
+# the top and bottom, and left and right, edges wrap to each other
+
+def get_locs(grid_width=20, lots_between=24):
+    mapping = defaultdict(list)
+    
+    # Avenue backyards
+    order = ["shop", "restaurant", "restaurant"]
+    for x in range(grid_width):
+        avenue1_curr = "street" if x % 4 == 0 else random.choice(order) 
+        avenue2_curr = "street" if x % 4 == 0 else random.choice(order)
+        mapping[avenue1_curr].append((x,0)) # BOTTOM OF GRID
+        mapping[avenue2_curr].append((x,lots_between-1)) # TOP OF GRID
+
+
+    # Residential areas
+    orderResidential = ["street", "house", "backyard", "house"]
+    for x in range(grid_width):
+        entity = orderResidential[x % len(orderResidential)]
+        for y in range (1, lots_between-1):
+            mapping[entity].append((x,y))
+
+    return mapping
+
+
