2e08c557acf5c23bf9728865ec9c0b98f51b3bb4
Daniel Mallia <Daniel.Mallia21@myhunter.cuny.edu>
2022-12-14 19:52:46 -0500

    Add mice dynamics
    Co-authored-by: Artjom-Plaunov <artyemnyc@gmail.com>

--- FoodModel.py
+++ FoodModel.py
@@ -7,6 +7,8 @@ GRID_PIXEL_HEIGHT = 1000
 GRID_WIDTH = 20
 GRID_HEIGHT = 24
 MINUTES_PER_TICK = 15
+# Ticks per average mouse arrival
+MAX_MOUSE_GROWTH_RATE = (48 * 60) / MINUTES_PER_TICK
 
 class StreetAgent(mesa.Agent):
     pass
@@ -18,7 +20,26 @@ class ShopAgent(mesa.Agent):
     pass
 
 class RestaurantAgent(mesa.Agent):
-    pass
+    def __init__(self, unique_id, model, initial_mice_pop, mouse_growth_rate):
+        super().__init__(unique_id, model)
+        self.mice_pop = 1 + np.random.poisson(initial_mice_pop)
+        self.mouse_growth_rate = np.random.poisson(
+            (mouse_growth_rate * 60) / MINUTES_PER_TICK)
+        self.ticks_until_new_mouse = np.random.poisson(self.mouse_growth_rate)
+        self.mice_caught = 0
+
+        # Probability of Interaction
+        self.mouse_prob = self.mice_pop / 100
+
+    def step(self):
+        self.ticks_until_new_mouse -= 1
+        if self.ticks_until_new_mouse <= 0:
+            self.mice_pop += 1
+            self.mouse_growth_rate = max(self.mouse_growth_rate - 1,
+                MAX_MOUSE_GROWTH_RATE)
+            self.mouse_prob = min(1, self.mice_pop / 100)
+            self.ticks_until_new_mouse = np.random.poisson(
+                self.mouse_growth_rate)
 
 class CatAgent(mesa.Agent):
     # @param hunger_rate Rate in hours until hungry
@@ -62,21 +83,24 @@ class CatAgent(mesa.Agent):
         # Time until they wake up
         self.ticks_until_awake = 0 if not self.is_asleep else np.random.poisson(self.sleep_duration_rate)
 
+        self.hunt_ability = self.random.uniform(0.5,1)
+
     def cat_encounter(self, other):
         pass
 
     # Looking for food...
-    # - Houses              [ ]
+    # - Houses              [X]
     # - Birds               [ ]
     # - Mice                [ ]
     def find_food(self):
         new_loc = None
         # Do a search of radius 1 for houses with food
         for neighbor in self.model.grid.iter_neighbors(self.pos, True, True):
-            if isinstance(neighbor, HouseAgent) and neighbor.food:
+            if (isinstance(neighbor, HouseAgent) and neighbor.food) or \
+                (isinstance(neighbor, RestaurantAgent) and neighbor.mice_pop > 0):
                 new_loc = neighbor.pos
                 self.found_food = neighbor
-                self.found_food_type = HouseAgent
+                self.found_food_type = type(neighbor)
                 break
 
         # Currently just random search if no food
@@ -156,10 +180,21 @@ class CatAgent(mesa.Agent):
 
         # Came here to eat? 
         if self.found_food:
+            food_success = False
             if self.found_food_type is HouseAgent:
                 self.is_hungry = False
                 self.found_food.food = False
-            self.ticks_until_hungry = np.random.poisson(self.hunger_rate)
+                food_success = True
+            elif self.found_food_type is RestaurantAgent:
+                u = self.random.uniform(0,1)
+                if (u < self.hunt_ability * self.found_food.mouse_prob):
+                    food_success = True
+                    self.is_hungry = False
+                    self.found_food.mice_pop -= 1
+                    self.found_food.mice_caught += 1
+                    self.found_food.mouse_growth_rate += 1
+            self.ticks_until_hungry = np.random.poisson(self.hunger_rate) \
+                if food_success else self.ticks_until_hungry
             #print("I ATE FOOD")
 
         # Wandering?
@@ -204,6 +239,10 @@ def get_hunger(model):
     return 0 if not num_cats else \
         (len([1 for cat in model.cat_list if cat.is_hungry]) / num_cats)
 
+def get_mice_pop(model):
+    restaurants = model.restaurant_list
+    return sum([res.mice_pop for res in restaurants])
+
 class_map = {
     "street" : StreetAgent,
     "house" : HouseAgent,
@@ -214,10 +253,10 @@ class_map = {
 
 class FoodModel(mesa.Model):
     def __init__(self, num_cats, hunger_rate, width, height, house_willingness,
-        house_rate, sleep_rate, sleep_duration_rate, cat_removal_rate=0,
-        seed=1234):
+        house_rate, sleep_rate, sleep_duration_rate, cat_removal_rate,
+        initial_mice_pop, mouse_growth_rate, seed=1234):
         self.current_tick = 1 # Time tracking for policies
-        self.cat_removal_rate = cat_removal_rate
+        self.cat_removal_rate = ((cat_removal_rate * 60) / MINUTES_PER_TICK)
         self.current_id = 1
         self.num_cats = num_cats
 
@@ -227,6 +266,7 @@ class FoodModel(mesa.Model):
 
         # Maintain a list of the cats - MUST be updated by reproduction
         self.cat_list = []
+        self.restaurant_list = []
 
         for i in range(self.num_cats):
             curr_a = CatAgent(self.next_id(), self, hunger_rate, sleep_rate,
@@ -242,24 +282,29 @@ class FoodModel(mesa.Model):
 
         # TODO: UPDATE THIS ACCORDINGLY
         environment_params={
-            "house" : (house_willingness, house_rate)
+            "house" : (house_willingness, house_rate),
+            "restaurant" : (initial_mice_pop, mouse_growth_rate)
         }
 
-        action_zones = ["house"]
+        action_zones = ["house", "restaurant"]
         for zone_type in environment:
             schedule = zone_type in action_zones
             for loc in environment[zone_type]:
+                curr_a = None
                 if zone_type in environment_params:
                     curr_a = class_map[zone_type](self.next_id(), self,
                         *environment_params[zone_type])
                 else:
                     curr_a = class_map[zone_type](self.next_id(), self)
+                if isinstance(curr_a, RestaurantAgent):
+                    self.restaurant_list.append(curr_a)
                 if schedule:
                     self.schedule.add(curr_a)
                 self.grid.place_agent(curr_a, loc)
 
         self.datacollector = mesa.DataCollector(
-            model_reporters={"Hunger": get_hunger})
+            model_reporters = { "Hunger"      : get_hunger,
+                                "Mice Pop."   : get_mice_pop})
 
     def step(self):
         """Advance the model by one step."""
@@ -343,8 +388,8 @@ def agent_portrayal(agent):
 if __name__ == "__main__":
     model_parameters = {"width" : GRID_WIDTH, "height" : GRID_HEIGHT,
         "cat_removal_rate" : mesa.visualization.Slider(
-            "Ticks until cat removal", value=0, min_value=0,
-            max_value=(30 * 24 * 60 / MINUTES_PER_TICK), step=1),
+            "Hours until cat removal", value=0, min_value=0,
+            max_value=(30 * 24), step=24),
         "num_cats" : mesa.visualization.Slider(
             "Number of cats", value=10, min_value=1, max_value=100, step=1),
         "hunger_rate" : mesa.visualization.Slider(
@@ -359,14 +404,22 @@ if __name__ == "__main__":
         "house_willingness" : mesa.visualization.Slider(
             "House willingness", value=0.2, min_value=0, max_value=1, step=0.1),
         "house_rate" : mesa.visualization.Slider(
-            "House rate (hours)", value=8, min_value=4, max_value=24, step=1)}
+            "House rate (hours)", value=8, min_value=4, max_value=24, step=1),
+        "initial_mice_pop" : mesa.visualization.Slider(
+            "Average initial mice population", value=2, min_value=1,
+            max_value=10, step=1),
+        "mouse_growth_rate" : mesa.visualization.Slider(
+            "Average mouse growth rate (hours)", value=72, min_value=48,
+            max_value=480, step=24)}
 
     grid = mesa.visualization.CanvasGrid(agent_portrayal, GRID_WIDTH,
         GRID_HEIGHT, GRID_PIXEL_WIDTH,GRID_PIXEL_HEIGHT)
     chart = mesa.visualization.ChartModule(
         [{"Label" : "Hunger", "Color" : "Black"}])
+    chart2 = mesa.visualization.ChartModule(
+        [{"Label" : "Mice Pop.", "Color" : "Black"}])
     server = mesa.visualization.ModularServer(
-        FoodModel, [grid,chart], "Food Model", model_parameters)
+        FoodModel, [grid, chart2], "Food Model", model_parameters)
     server.launch()
 
 
